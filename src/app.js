f1f76e2cf80392860ca76cb9de09321ddff6a942
Johana.Usaquen <lizethwist694@gmail.com>
2024-10-12 22:35:02 -0500

    Se crean los archivos necesarios para poder inicar el servidor y para la DB

--- src/app.js
+++ src/app.js
@@ -3,19 +3,27 @@
 const express = require('express'); //Aqui lo requerimos
 const path = require('path'); //para no llenarme de rutas
 const morgan = require('morgan');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const myConnection = require('express-myconnection');
-
 const app = express();//Incializamos con una constante 
 
+//importaando rutas
+const customerRoutes = require('./routes/customer');
+
+
 //Configurar express
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3000
+)
+
+
 //Configuramos el motor de plantillas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
 //middlewares, funciones que se ejecutan antes de las peticiones de los usuariso
 app.use(morgan('dev'));
+
 
 //conectar a mysql
 app.use(myConnection(mysql,{
@@ -27,11 +35,24 @@ app.use(myConnection(mysql,{
 }, 'single'));
 
 //rutas
+app.use('/', customerRoutes)
 
-app.listen(app.get('port'), () => {
-    console.log('server on port 3000')
-}); //Incializamos el servidor, cuando inicie el servidar mostrara 
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//Otra configuración para poder usar local host 3000
+app.listen(3000, () => {
+    console.log('server on port 3000');
+  });
+
+
+//emepzando el servidor
+ //Incializamos el servidor, cuando inicie el servidar mostrara 
 //un mensaje que nos diga que el servidor esta funcionando 
+
+
 
 //en la consola con node src/app.js verificamos el funcionamiento
 
